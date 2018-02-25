1ce52d86d9a2e5fbf4b77487dd4a8811483fffab
Habier <Habier@users.noreply.github.com>
2018-02-25 12:25:14 +0100

    phpdoc improvements

--- src/Resizer.php
+++ src/Resizer.php
@@ -4,25 +4,41 @@ namespace Habier\JPGResizer;
 
 class Resizer
 {
-    public static function resize($destinyFile, $originalFile, $newwidth, $newheight, $width = NULL, $height = NULL)
+    /**
+     * Resize and save a jpg to the given sizes
+     * @param string $destinyFile
+     * @param string $originalFile
+     * @param int $newwidth
+     * @param int $newheight
+     * @param int|null $OriginalWidth if left null it will calculate itself
+     * @param int|null $originalHeight if left null it will calculate itself
+     */
+    public static function resize($destinyFile, $originalFile, $width, $height, $OriginalWidth = NULL, $originalHeight = NULL)
     {
-        if (is_null($width) || is_null($height))
-            list($width, $height) = getimagesize($originalFile);
+        if (is_null($OriginalWidth) || is_null($originalHeight))
+            list($OriginalWidth, $originalHeight) = getimagesize($originalFile);
 
-        $thumb = imagecreatetruecolor($newwidth, $newheight);
+        $thumb = imagecreatetruecolor($width, $height);
         $source = imagecreatefromjpeg($originalFile);
 
         // Resize
-        imagecopyresized($thumb, $source, 0, 0, 0, 0, round($newwidth), round($newheight), $width, $height);
+        imagecopyresized($thumb, $source, 0, 0, 0, 0, $width, $height, $OriginalWidth, $originalHeight);
 
         imagejpeg($thumb, $destinyFile);
         imagedestroy($thumb);
     }
 
+    /**
+     * Create a thumbnail limiting its bigger side but preserving aspect ratio.
+     * @param string $destinyFile
+     * @param string $originalFile
+     * @param int $sizelimit
+     * @return bool True if thumbnail created, false if thumbnail would be bigger than original
+     */
     public static function autoResize($destinyFile, $originalFile, $sizelimit)
     {
         list($width, $height) = getimagesize($originalFile);
-        //Check for new sizes
+
         if ($width >= $height && $width > $sizelimit) {
             $newwidth = $sizelimit;
             $newheight = $height / ($width / $newwidth);
@@ -30,11 +46,11 @@ class Resizer
             $newheight = $sizelimit;
             $newwidth = $width / ($height / $newheight);
         } else {
-            return false; //I dont want to upscale the image
+            return false; //thumbnail must not be bigger than original
         }
 
-        Resizer::resize($destinyFile, $originalFile, $newwidth, $newheight, $width, $height);
-        return true;//thumbnail created without upscaling
+        self::resize($destinyFile, $originalFile, round($newwidth), round($newheight), $width, $height);
+        return true;
     }
 
 }
